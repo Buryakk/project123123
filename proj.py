bab82461b62c852900789fdb3fbf29be7a6c644c
Buryakk <93859854+Buryakk@users.noreply.github.com>
2021-12-04 19:53:32 +0200

    Add files via upload

--- proj.py
+++ proj.py
@@ -5,34 +5,23 @@ height = 500
 window = display.set_mode((width, height)) 
 display.set_caption('123')
 
-class GameSprite(sprite.Sprite):
-    def init(self, player_image, player_x, player_y, size_x, size_y, player_speed): 
-        sprite.Sprite.init(self)
+bf = transform.scale(image.load('bf.jpg'), (700, 500))
+r1 = transform.scale(image.load('rr1.png'), (170, 30))
 
-    self.image = transform.scale(image.load(player_image), (size_x, size_y))
-    self.speed = player_speed
-    self.rect = self.image.get_rect()
-    self.rect.x = player_x
-    self.rect.y = player_y
-def reset(self):
-    window.blit(self.image, (self.rect.x, self.rect.y))
-
-
-x = 10
+x = 290
 y = 10
+x3 = 290
+y3 = 490
+x2 = 0
+y2 = 0
 
-
-class Player(GameSprite):
+class Player():
     def update(self):
-        keys_pressed = key.get_pressed()
+        keys_pressed = key.get_pressed(r1)
         if keys_pressed[K_LEFT]:
              x -= 10
         if keys_pressed[K_RIGHT]:
              x += 10
-
-
-
-
 finish = False
 
 game = True
@@ -40,11 +29,11 @@ game = True
 speed_x = 3 
 speed_y = 2
 
-while game: 
-    ball.rect.x += speed_x 
-    ball.rect.y += speed_y
-
-for e in event.get():
-    if e.type == QUIT:
-        game = False
-display.update()
+while game:
+    window.blit(bf, (x2, y2))
+    window.blit(r1, (x, y))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+time.delay(100)
